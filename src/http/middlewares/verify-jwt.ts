047f9aed8ecbf61d3dbe190b93e095f69a80e1f1
codesofC <inelusjudelin01@gmail.com>
2025-03-22 15:47:37 -0300

    jinelus : feat add JWT verification middleware for authentication

---
+++ src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,10 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+
+export const verifyJWT = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        await request.jwtVerify()
+    } catch (error) {
+        return reply.status(401).send({ message: 'Unauthorized' })
+    }
+}
