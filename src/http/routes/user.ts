1cf134f7b79d604eade52fe947a12b29b98182b9
codesofC <inelusjudelin01@gmail.com>
2025-03-22 15:48:51 -0300

    jinelus : feat add edit user route with JWT verification

--- src/http/routes/user.ts
+++ src/http/routes/user.ts
@@ -1,11 +1,15 @@
 import type { FastifyInstance } from "fastify";
 import { createUserController } from "../controllers/users/create-user-controller";
 import { getUserByIdController } from "../controllers/users/get-user-by-id-controller";
+import { editUserController } from "../controllers/users/edit-user-controller";
+import { verifyJWT } from "../middlewares/verify-jwt";
 
 
-export async function usersRouter(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
 
     app.post('/users', createUserController)
     app.get('/users/:userId', getUserByIdController)
+
+    app.put('/users', { onRequest: [verifyJWT] } ,editUserController)
     
 }
