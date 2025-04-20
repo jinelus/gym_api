7fe938f16caae85d2a0ae3f27da1048c90a21ae5
codesofC <inelusjudelin01@gmail.com>
2025-04-20 15:34:45 -0300

    feat add EditUserUseCase and corresponding tests

---
+++ src/uses-cases/user/edit-user.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { EditUserUseCase } from "./edit-user";
+
+describe('Edit user use case', () => {
+
+    let inMemoryUsersRepository: InMemoryUsersRepository
+    let sut: EditUserUseCase
+
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository()
+        sut = new EditUserUseCase(inMemoryUsersRepository)
+    })
+
+    it('should be able to edit a user', async () => {
+        inMemoryUsersRepository.users.push({
+            id: 'user_01',
+            name: 'John Doe',
+            email: 'a5o8o@example.com',
+            password: '123456',
+            gymId: 'gym_01',
+            role: 'MEMBER',
+            createdAt: new Date(),
+            updatedAt: null,
+        })
+
+        const response = await sut.execute({
+            userId: 'user_01',
+            email: 'aaab@gmail.com',
+            gymId: 'gym_01',
+            name: 'Johnattan Gilbert',
+        })
+
+        expect(response).toEqual({
+            user: expect.objectContaining({
+                email: 'aaab@gmail.com',
+                name: 'Johnattan Gilbert',
+            })
+        })
+    })
+})
---
+++ src/uses-cases/user/edit-user.ts
@@ -0,0 +1,32 @@
+import { UsersRepository } from "@/repositories/interfaces/users-repository";
+import type { User } from "@prisma/client";
+import { RessourceNotFoundError } from "../error/ressource-not-found-error";
+
+export interface EditUserUseCaseProps {
+    userId: string
+    name: string
+    email: string
+    gymId: string
+}
+
+interface EditUserUseCaseResponse {
+    user: User
+}
+
+export class EditUserUseCase {
+    constructor(private usersRepository: UsersRepository){}
+
+    async execute({ userId, email, gymId, name }: EditUserUseCaseProps): Promise<EditUserUseCaseResponse> {
+        const userFound = await this.usersRepository.findById(userId)
+
+        if(!userFound) {
+            throw new RessourceNotFoundError()
+        }
+
+        const user = await this.usersRepository.update({ userId, email, gymId, name })
+
+        return {
+            user
+        }
+    }
+}
